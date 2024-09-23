6f1251fd2b02720ef74ba005eaaba4221c0fe7d4
Евгений Терехов <nergal@psu.ru>
2024-09-23 10:14:22 +0400

    chore: initial setup

---
+++ src/test/java/ru/terekhov/package-info.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright Eugene Terekhov.
+ *
+ * This file is part of pl-essentials.
+ *
+ * pl-essentials is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3
+ * or any later version as published by the Free Software Foundation.
+ *
+ * pl-essentials is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with pl-essentials.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Root package for the module.
+ */
+package ru.terekhov;
